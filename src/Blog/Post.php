bfb5698d6692b18879b7c32f6e9f2faaa7a8a580
AlexBarnhouse <aragon-1995@mail.ru>
2023-03-15 06:07:38 +0500

    lesson-2

--- src/Blog/Post.php
+++ src/Blog/Post.php
@@ -13,6 +13,63 @@ class Post
     {
     }
 
+    /**
+     * @return int
+     */
+    public function id(): int
+    {
+        return $this->id;
+    }
+
+
+    /**
+     * @return User
+     */
+    public function author(): User
+    {
+        return $this->author;
+    }
+
+    /**
+     * @param User $author
+     */
+    public function setAuthor(User $author): void
+    {
+        $this->author = $author;
+    }
+
+    /**
+     * @return string
+     */
+    public function title(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     */
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return string
+     */
+    public function text(): string
+    {
+        return $this->text;
+    }
+
+    /**
+     * @param string $text
+     */
+    public function setText(string $text): void
+    {
+        $this->text = $text;
+    }
+
     public function __toString()
     {
         return $this->title . ' >>> ' .$this->text;
--- src/Blog/Repositories/UsersRepository/UserNotFoundException.php
+++ src/Blog/Repositories/UsersRepository/UserNotFoundException.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace GeekBrains\Blog\Repositories\UsersRepository;
+namespace GeekBrains\LevelTwo\Blog\Repositories\UsersRepository;
 
 use GeekBrains\LevelTwo\Blog\Exceptions\AppException;
 class UserNotFoundException extends AppException
